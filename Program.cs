6956e1c25b4942d68976875b00f2dccc9984681a
Jenry Sanchez <henryfuerte560@gmail.com>
2024-05-18 18:20:47 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("escriba su nombre");
+var nombre = Console.ReadLine();
+Console.WriteLine("escriba su edad");
+var edad = int.Parse(Console.ReadLine());
+
+
+if (edad > 18)
+{
+    Console.WriteLine("usted es mayor a 18");
+}
+else
+{
+    Console.WriteLine("Usted es menor a 18");
+}
+
+for (int i = 0; i < 10; i++)
+{
+   Console.WriteLine(i+1);
+}
